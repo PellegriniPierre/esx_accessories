28c3b49672aae6c1f34de6d8f9720a41bc7ef3f3
Pierre Pellegrini <pellegrini.pierre@gmail.com>
2021-03-08 07:58:45 +0100

    Maj sql + config

--- localization/fr_esx_accessories.sql
+++ localization/fr_esx_accessories.sql
@@ -5,8 +5,7 @@ INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
 ('society_ambulance', 'Ambulance', 1),
 ('society_police', 'Police', 1),
 ('society_taxi', 'Taxi', 1),
-('user_ears', 'Ears', 0),
-('user_glasses', 'Glasses', 0),
-('user_helmet', 'Helmet', 0),
-('user_mask', 'Mask', 0)
-;
+('user_ears', "Boucles d'oreilles", 0),
+('user_glasses', 'Lunettes', 0),
+('user_helmet', 'Casques', 0),
+('user_mask', 'Masques', 0);
